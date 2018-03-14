52071ba4cd29c333e5e112f917bd96b5f0b49818
David Ruppe <david.ruppe@virginmedia.com>
2018-03-14 15:42:31 +0000

    btn click event handlers

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // declare variables
 
+var score, turnScore;
+
 // create functions
 
 // event handlers
@@ -8,15 +10,22 @@
 // click button to fight
 // click button to save
 
-var hero1 = Math.floor(Math.random() * 2) + 1;		// generates a random number (either zero or one)
-var hero2 = Math.floor(Math.random() * 2) + 1;
+document.querySelector('.btn-fight').addEventListener('click', function() {
+	var hero1 = Math.floor(Math.random() * 2) + 1;		// generates a random number (either zero or one)
+	var hero2 = Math.floor(Math.random() * 2) + 1;
 
-document.getElementById('pic-1').style.display = 'block';
-document.getElementById('pic-2').style.display = 'block';
+	document.getElementById('pic-1').style.display = 'block';
+	document.getElementById('pic-2').style.display = 'block';
 
-document.getElementById('pic-1').src = 'images/hero-' + hero1 + '.jpg';		//2. display the result
-document.getElementById('pic-2').src = 'images/hero-' + hero2 + '.jpg';		
-
-//3. Update the round score IF the rolled number was NOT a 0 ...
+	document.getElementById('pic-1').src = 'images/hero-' + hero1 + '.jpg';		//2. display the result
+	document.getElementById('pic-2').src = 'images/hero-' + hero2 + '.jpg';		
+}
+//3. Update the round score IF the number was NOT a 0 ...
 
 // next player
+
+document.querySelector('.btn-save').addEventListener('click', function() {
+
+
+
+document.querySelector('.btn-new').addEventListener('click', newGame);
