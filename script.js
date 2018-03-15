a1b5da066518243388fa088e056bfb922a68d737
David Ruppe <david.ruppe@virginmedia.com>
2018-03-15 12:34:28 +0000

    New content

--- script.js
+++ script.js
@@ -11,20 +11,70 @@ var score, turnScore;
 // click button to save
 
 document.querySelector('.btn-fight').addEventListener('click', function() {
-	var hero1 = Math.floor(Math.random() * 2) + 1;		// generates a random number (either zero or one)
-	var hero2 = Math.floor(Math.random() * 2) + 1;
+	if (gamePlaying) {
+		var hero1 = Math.floor(Math.random() * 2) + 1;		// generates a random number (either zero or one)
+		var hero2 = Math.floor(Math.random() * 2) + 1;
 
-	document.getElementById('pic-1').style.display = 'block';
-	document.getElementById('pic-2').style.display = 'block';
+		document.getElementById('pic-1').style.display = 'block';
+		document.getElementById('pic-2').style.display = 'block';
 
-	document.getElementById('pic-1').src = 'images/hero-' + hero1 + '.jpg';		//2. display the result
-	document.getElementById('pic-2').src = 'images/hero-' + hero2 + '.jpg';		
-}
-//3. Update the round score IF the number was NOT a 0 ...
+		document.getElementById('pic-1').src = 'images/hero-' + hero1 + '.jpg';		//2. display the result
+		document.getElementById('pic-2').src = 'images/hero-' + hero2 + '.jpg';		
 
-// next player
+//3. Update the players turn score IF the number was NOT a 0 ...
+
+			if (hero1 !== 7 && hero2 !== 7) {
+
+			if (hero1 !== 0 && hero2 !== 0) {		// ... or 7 ...
+
+				if (hero1 !== 8 && hero2 !== 8) {		// ... or 8 ...
+					turnScore += hero1 + hero2;
+					document.querySelector('#current-' + activePlayer).textContent = turnScore;
+				}
+				else {
+					scores[activePlayer] = 0;		//loses complete score
+					document.querySelector('#score-' + activePlayer).textContent = score[activePlayer];
+					nextPlayer();
+				}
+			}
+				else {
+					roundScore = 0;
+					document.querySelector('#current-' + activePlayer).textContent = roundScore;
+				}
+		}
+		else {
+			nextPlayer();
+		}
+	}
+
+});
 
 document.querySelector('.btn-save').addEventListener('click', function() {
+
+	if (gamePlaying) {
+			//add current score to global score
+			score[activePlayer] += turnScore;
+
+			//update the UI
+			document.querySelector('#score-' + activePlayer).textContent = score[activePlayer];
+
+		// check if player won the game
+		if (scores[activePlayer] >= winningScore) {
+			document.querySelector('#name-' + activePlayer).textContent = 'Winner';
+			
+			document.getElementById('dice-1').style.display = 'none';
+			document.getElementById('dice-2').style.display = 'none';
+
+			document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+			document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+			gamePlaying = false;
+		} else {
+			nextPlayer();
+		}
+	}
+	
+});
+
 
 
 
